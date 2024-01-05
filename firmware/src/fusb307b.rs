29a4f9b9d4a8bc32942e2a9751bf87d454df025d
Zain Ahmed <zainahmed1559@gmail.com>
2024-01-05 00:13:41 -0500

    gross wip test code, goodCRC works!

--- firmware/src/fusb307b.rs
+++ firmware/src/fusb307b.rs
@@ -2,4 +2,11 @@
 pub enum Register {
     CCSTAT = 0x1D,
     RESET = 0xA2,
+    SINK_TRANSMIT = 0x40,
+    TRANSMIT = 0x50,
+    RXDETECT = 0x2F,
+    RXBYTECNT = 0x30,
+    TCPC_CTRL = 0x19,
+    FAULTSTAT = 0x1F,
+    ROLECTRL = 0x1A,
 }
--- firmware/src/main.rs
+++ firmware/src/main.rs
@@ -85,7 +85,51 @@ fn main() -> ! {
     i2c.write(fusb_address, &[fusb307b::Register::RESET as u8, 0x01])
         .unwrap();
 
-    delay.delay_ms(100u32);
+    delay.delay_ms(10u32);
+
+    // Check for "chip has reset fault"
+    let mut faultstat: [u8; 1] = [0x00];
+    i2c.write_read(
+        fusb_address,
+        &[fusb307b::Register::FAULTSTAT as u8],
+        &mut faultstat,
+    )
+    .unwrap();
+    println!("FAULTSTAT: {:08b}", faultstat[0]);
+
+    // Clear the "chip has reset" fault
+    println!("Clearing faultstat");
+    i2c.write(
+        fusb_address,
+        &[fusb307b::Register::FAULTSTAT as u8, 0b10000000],
+    )
+    .unwrap();
+
+    // Read faultstat again
+    i2c.write_read(
+        fusb_address,
+        &[fusb307b::Register::FAULTSTAT as u8],
+        &mut faultstat,
+    )
+    .unwrap();
+    println!("FAULTSTAT: {:08b}", faultstat[0]);
+
+    // Tell this thing it's not a dual role port
+    i2c.write(
+        fusb_address,
+        &[fusb307b::Register::ROLECTRL as u8, 0b00001010],
+    )
+    .unwrap();
+
+    // Print out rolectrl register
+    let mut rolectrl: [u8; 1] = [0x00];
+    i2c.write_read(
+        fusb_address,
+        &[fusb307b::Register::ROLECTRL as u8],
+        &mut rolectrl,
+    )
+    .unwrap();
+    println!("ROLECTRL: 0b{:08b}", rolectrl[0]);
 
     // Read cable orientation
     let mut ccstat: [u8; 1] = [0x00];
@@ -100,13 +144,37 @@ fn main() -> ! {
 
     println!("CC1: {:#b}\tCC2: {:#b}", cc1_stat, cc2_stat);
 
+    // Tell the TCPC which CC pin to use for comms
     if cc1_stat != 0 {
         println!("CC1 connected!");
+        i2c.write(fusb_address, &[fusb307b::Register::TCPC_CTRL as u8, 0x00])
+            .unwrap();
     } else if cc2_stat != 0 {
         println!("CC2 connected!");
+        i2c.write(fusb_address, &[fusb307b::Register::TCPC_CTRL as u8, 0x01])
+            .unwrap();
     } else {
         println!("No CC connection!");
     }
 
-    loop {}
+    // // Enable transmission as a sink with retry counter set to 3x
+    // let sink_tx_cfg: u8 = 0b00110000;
+    // println!("Enabling sink tx");
+    // i2c.write(fusb_address, &[fusb307b::Register::SINK_TRANSMIT as u8, sink_tx_cfg]).unwrap();
+
+    println!("Enabling SOP rx detection and auto goodcrc");
+    i2c.write(fusb_address, &[fusb307b::Register::RXDETECT as u8, 0x01])
+        .unwrap();
+
+    loop {
+        // let mut rx_byte_count: [u8; 1] = [0x00];
+        // i2c.write_read(fusb_address, &[fusb307b::Register::RXBYTECNT as u8], &mut rx_byte_count).unwrap();
+        // println!("rx byte count: {:?}", rx_byte_count);
+
+        // if rx_byte_count[0] > 0 {
+        //     for _ in 0..rx_byte_count[0] {
+
+        //     }
+        // }
+    }
 }

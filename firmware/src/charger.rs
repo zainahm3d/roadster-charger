0730431c6a8aced53ee39afb801ccc5a33e258b2
Zain Ahmed <zainahmed1559@gmail.com>
2025-03-09 13:06:59 -0400

    get rid of extra variable for max output

--- firmware/src/charger.rs
+++ firmware/src/charger.rs
@@ -38,7 +38,6 @@ pub struct State {
 
     output_mv: u32,
     output_ma: u32,
-    max_output_ma: u32,
 
     pub pdo_mv: u32,
     pub pdo_ma: u32,
@@ -46,15 +45,19 @@ pub struct State {
 
 impl State {
     // Calculate max charge current based on charger PDO and current temperature.
-    fn update_max_charge_current(&mut self) {
-        // Only output 85% of brick's advertised capabilities to account for
-        // power dissapated in cable and our own 92ish % efficiency.
-        let pdo_mw = self.pdo_ma * self.pdo_mv / 1000;
-        let max_output_mw = (pdo_mw as f32 * 0.85) as u32;
+    fn update_target_ma(&mut self) {
+        if self.output_mv > MIN_BATTERY_VOLTAGE_MV {
+            // Only output 90% of brick's advertised capabilities to account for
+            // power dissapated in cable and our own 92ish % efficiency.
+            let pdo_mw = self.pdo_ma as f32 * self.pdo_mv as f32 / 1000.0;
+            let max_output_mw = pdo_mw * 0.9;
 
-        // Based on PDO and current output voltage, never charge at > 2A
-        esp_println::dbg!(max_output_mw / self.output_mv * 1000);
-        self.max_output_ma = (max_output_mw / self.output_mv * 1000).clamp(0, 2_000);
+            // Based on PDO and current output voltage, never charge at > 2A
+            self.target_ma =
+                ((max_output_mw / self.output_mv as f32 * 1000.0).clamp(0.0, 2_000.0)) as u32;
+        } else {
+            self.target_ma = 0;
+        }
     }
 }
 
@@ -71,7 +74,6 @@ impl Default for State {
             input_ma: 0,
             output_mv: 0,
             output_ma: 0,
-            max_output_ma: 0,
             pdo_mv: 0,
             pdo_ma: 0,
         }
@@ -95,8 +97,7 @@ pub fn run<T: I2c, P: OutputPin>(i2c: &mut T, enable_pin: &mut P, s: &mut State)
 
     match s.mode {
         Mode::ConstantCurrent => {
-            s.update_max_charge_current();
-            s.target_ma = s.target_ma.clamp(0, s.max_output_ma);
+            s.update_target_ma();
             let p_error = s.target_ma as i32 - s.output_ma as i32;
 
             // Slew rate limit only in positive direction
@@ -106,8 +107,10 @@ pub fn run<T: I2c, P: OutputPin>(i2c: &mut T, enable_pin: &mut P, s: &mut State)
             s.duty = (s.duty as i32 + duty_shift).clamp(0, boost::DAC_MAX_OUTPUT as i32) as u16;
 
             if s.output_mv >= CV_TARGET_MV {
+                s.target_ma = 0;
                 s.mode = Mode::ConstantVoltage;
             } else if s.output_ma < MIN_CURRENT_MA {
+                s.target_ma = 0;
                 s.mode = Mode::Disabled; // Battery is gone!
                 s.tick_disabled = s.tick;
             } else {
